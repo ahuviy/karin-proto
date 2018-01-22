876b3b58892e692e12c1a3f1ce636f019e991285
Ahuvi <ahuvi.yearim@comm-it.com>
2018-01-22 09:04:58 +0200

    materials service is now tied to materials page

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { MaterialsRouting } from './materials.routing';
 import { MaterialsPage } from './materials.page';
-import { MaterialsService } from './services/materials.service';
 import { MaterialsTableComponent } from './materials-table/materials-table.component';
 import { TableHeaderComponent } from './materials-table/table-header/table-header.component';
 import { TableRowComponent } from './materials-table/table-row/table-row.component';
@@ -20,9 +19,6 @@ import { AddMaterialComponent } from './add-material/add-material.component';
     imports: [
         SharedModule,
         MaterialsRouting,
-    ],
-    providers: [
-        MaterialsService,
     ],
 })
 export class MaterialsModule { }
--- src/app/materials/materials.page.ts
+++ src/app/materials/materials.page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MaterialsService } from 'app/materials/services/materials.service';
 
 @Component({
     selector: 'kp-materials-page',
     styleUrls: ['./materials.page.scss'],
     templateUrl: './materials.page.html',
+    providers: [
+        MaterialsService,
+    ],
 })
 export class MaterialsPage {
     
